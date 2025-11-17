92759621c6f55d20b38954c6bf8bbd5f9896d41a
romulo-donadoni <romulogdonadoni@outlook.com>
2025-11-16 21:04:14 -0300

    feat(loading): update loading component to use Loader from lucide-react and enhance styling

--- src/app/(app)/[tenantId]/loading.tsx
+++ src/app/(app)/[tenantId]/loading.tsx
@@ -1,7 +1,9 @@
+import { Loader } from "lucide-react";
+
 export default function Loading() {
     return (
-        <div>
-            Carregando...
+        <div className="flex flex-col flex-1 items-center justify-center">
+            <Loader size={40} className="animate-spin" />
         </div>
     )
 }
